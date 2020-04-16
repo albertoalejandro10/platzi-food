cb7988667a0c21e139f2ed91e156513927bd8aa2
Alberto Alejandro <alberto196g@gmail.com>
2020-04-16 02:48:50 -0400

    Practicando esta complicado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
   plugins: [],
 }
