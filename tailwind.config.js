2e472f267e561c48170a3f59fa5fa80e68a40693
Alberto Alejandro <alberto196g@gmail.com>
2020-04-17 03:24:42 -0400

    Agregado header, main y footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem'
+      }
+    }
   },
-  variants: {
-    opacity: ['responsive', 'hover', 'focus', 'disabled'],
-    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-  },
-  plugins: [],
+  variants: {},
+  plugins: []
 }
